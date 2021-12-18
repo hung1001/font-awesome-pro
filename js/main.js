9c44c7c6831ec2365e5aad7478c939c0e01bccdb
hung1001 <hoanghung96cs@gmail.com>
2021-12-18 17:39:41 +0700

    Replace deprecated method

--- js/main.js
+++ js/main.js
@@ -7,20 +7,20 @@ let last_page = false,
 while (!last_page) {
   $.ajax({
     url: `https://api.github.com/repos/${REPO}/commits?page=${page}`,
-    dataType: "json",
+    dataType: 'json',
     async: false,
-    success: data => {
+    success: (data) => {
       if (data.length > 0) {
-        data.forEach(child => listCommit.push(child));
+        data.forEach((child) => listCommit.push(child));
         page++;
       } else {
         last_page = true;
       }
-    }
+    },
   });
 }
 
-const listVersions = listCommit.map(arr => {
+const listVersions = listCommit.map((arr) => {
   let regex = arr.commit.message.match(/\d+(\.\d+)+/g);
   if (regex != null) {
     arr.sha = arr.sha.slice(0, 7);
@@ -30,45 +30,62 @@ const listVersions = listCommit.map(arr => {
 });
 
 const removeDuplicate = listVersions.reduce((unique, value) => {
-  if (!unique.some(obj => (obj.ver === value.ver))) {
+  if (!unique.some((obj) => obj.ver === value.ver)) {
     unique.push(value);
   }
   return unique;
 }, []);
 
-removeDuplicate.forEach(value => {
+removeDuplicate.forEach((value) => {
   let { sha, ver } = value;
   if (ver) {
     $('#list-versions').append(`<option value='${sha}'>v${ver}</option>`);
-    $('#archive').append(`<option data-link='https://github.com/${REPO}/archive/v${ver}.zip'>v${ver}</option>`);
+    $('#archive').append(
+      `<option data-link='https://github.com/${REPO}/archive/v${ver}.zip'>v${ver}</option>`
+    );
   }
 });
 
-$('.js-select2').map(function(index, elem) {
+$('.js-select2').map(function (index, elem) {
   $(elem).select2({
     width: '100%',
-    placeholder: $(elem).attr('data-placeholder') && $(elem).attr('data-placeholder')
+    placeholder: $(elem).attr('data-placeholder') && $(elem).attr('data-placeholder'),
   });
 });
 
 const { sha, ver } = removeDuplicate[0];
 
 $('.ver').text(ver);
-$('#a').val(`<link href="https://cdn.jsdelivr.net/gh/${REPO}@${sha}/css/all.css" rel="stylesheet" type="text/css" />`);
-$('#c').val(`<link href="https://cdn.staticaly.com/gh/${REPO}/${sha}/css/all.css" rel="stylesheet" type="text/css" />`);
-$('#e').val(`<link href="https://rawcdn.githack.com/${REPO}/${sha}/css/all.css" rel="stylesheet" type="text/css" />`);
+$('#a').val(
+  `<link href="https://cdn.jsdelivr.net/gh/${REPO}@${sha}/css/all.css" rel="stylesheet" type="text/css" />`
+);
+$('#c').val(
+  `<link href="https://cdn.staticaly.com/gh/${REPO}/${sha}/css/all.css" rel="stylesheet" type="text/css" />`
+);
+$('#e').val(
+  `<link href="https://rawcdn.githack.com/${REPO}/${sha}/css/all.css" rel="stylesheet" type="text/css" />`
+);
 $('head').append($('#a').val());
 
-$('body').on('select2:select', '#list-versions', function() {
-  let newSHA = $(this).val();
-  const replaceSHA = (ele, sha = newSHA) => $(ele).val().replace(/[a-f0-9]{7}\//i, `${sha}/`);
+const replaceSHA = ({ element, sha }) =>
+  $(element).val(
+    $(element)
+      .val()
+      .replace(/[a-f0-9]{7}\//i, `${sha}/`)
+  );
 
-  $('#a').val(replaceSHA('#a'));
-  $('#c').val(replaceSHA('#c'));
-  $('#e').val(replaceSHA('#e'));
+$('body').on('select2:select', '#list-versions', function () {
+  const newSHA = $(this).val();
+
+  ['#a', '#c', '#e'].forEach(function (element) {
+    replaceSHA({
+      element,
+      sha: newSHA,
+    });
+  });
 });
 
-$('body').on('select2:select', '#archive', function() {
+$('body').on('select2:select', '#archive', function () {
   window.open($(this).select2('data')[0].element.dataset.link, '_blank');
 });
 
@@ -76,31 +93,31 @@ const copyObj = {
   item: {
     jsdelivr: {
       input: 'a',
-      btn: 'b'
+      btn: 'b',
     },
     staticaly: {
       input: 'c',
-      btn: 'd'
+      btn: 'd',
     },
     githack: {
       input: 'e',
-      btn: 'f'
-    }
+      btn: 'f',
+    },
   },
-  copy: function() {
+  copy: function () {
     for (let key in this.item) {
       if (this.item.hasOwnProperty(key)) {
         let btn = `#${this.item[key].btn}`,
           input = `#${this.item[key].input}`;
-        $('body').on('click', btn, function() {
-          if ($(input).val().length > 0) {
+        $('body').on('click', btn, function () {
+          if ($(input).val().length > 0 && navigator?.clipboard) {
             $(input).select();
-            document.execCommand('copy');
+            navigator.clipboard.writeText($(input).val());
           }
         });
       }
     }
-  }
+  },
 };
 
 copyObj.copy();
